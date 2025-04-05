4e7bf11b75580c92b58c64f772236c797a8fb046
SamueldeJeusDeOliveira <samuksjj@gmail.com>
2025-04-04 21:41:14 -0300

    LINQ com Lambda

---
+++ LINQ/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LINQ.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
